7271934f4ae4f43b0f11d38c091f5ccff739dcaa
BannerBomb <BannerBomb55@gmail.com>
2019-06-21 16:53:57 -0400

    removed un-needed code.
    removed un-needed code.

--- commands/administration/addrole.js
+++ commands/administration/addrole.js
@@ -26,8 +26,8 @@ module.exports = class addroleCommand extends LenoxCommand {
 		const user = msg.mentions.members.first();
 		const foundRole = msg.guild.roles.find(role => role.name.toLowerCase() === args.slice(1).join(' ').toLowerCase());
 
-		if (msg.mentions.members.size < 1) return msg.reply(lang.addrole_nomention);
-		if (addedrole.length < 1) return msg.reply(lang.addrole_norolename);
+		if (!msg.mentions.members.size) return msg.reply(lang.addrole_nomention);
+		if (!addedrole.length) return msg.reply(lang.addrole_norolename);
 		if (!foundRole) return msg.reply(lang.addrole_rolenotexist);
 		if (user.roles.has(foundRole.id)) return msg.reply(lang.addrole_memberalreadyhasrole);
 
