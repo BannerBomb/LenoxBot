e59b5c8746d0c19c8bfdc795c0a6e83ae863800d
BannerBomb <BannerBomb55@gmail.com>
2019-06-21 17:07:03 -0400

    removed un-needed code.
    removed un-needed code.

--- commands/administration/removerole.js
+++ commands/administration/removerole.js
@@ -26,8 +26,8 @@ module.exports = class removeroleCommand extends LenoxCommand {
 		const user = msg.mentions.members.first();
 		const foundRole = msg.guild.roles.find(role => role.name.toLowerCase() === args.slice(1).join(' ').toLowerCase());
 
-		if (msg.mentions.members.size < 1) return msg.reply(lang.removerole_nomention);
-		if (addedrole.length < 1) return msg.reply(lang.removerole_norolename);
+		if (!msg.mentions.members.size) return msg.reply(lang.removerole_nomention);
+		if (!addedrole.length) return msg.reply(lang.removerole_norolename);
 		if (!foundRole) return msg.reply(lang.removerole_rolenotexist);
 		if (!user.roles.has(foundRole.id)) return msg.reply(lang.removerole_error);
 
